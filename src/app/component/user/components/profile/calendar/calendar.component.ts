7a2018dce11a8e4c1ae8c23dcb20eb903303548a
Vitalii Politylo <vitaliipolitylo99@gmail.com>
2020-11-25 17:51:05 +0200

    Added getting active habits to the list

--- src/app/component/user/components/profile/calendar/calendar.component.ts
+++ src/app/component/user/components/profile/calendar/calendar.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from '@language-service/language.service';
 import { CalendarBaseComponent } from '@shared/components';
+import { calendarIcons } from 'src/app/image-pathes/calendar-icons';
+import { HabitAssignService } from '@global-service/habit-assign/habit-assign.service';
 
 @Component({
   selector: 'app-calendar',
@@ -9,9 +11,50 @@ import { CalendarBaseComponent } from '@shared/components';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent extends CalendarBaseComponent implements OnInit, OnDestroy {
+  public isHabitsPopUpOpen = false;
+  public selectedDay;
+  public habitsCalendarSelectedDate;
+  public calendarIcons = calendarIcons;
+  public userHabitAssigns;
+  public habitEnrollDates;
+  public formatedData: string;
+  public isChangingEnroll: boolean = false;
+  public habits2: any[] = [];
+  public habits = [
+        {
+          title: "Use own cup",
+          completed: false,
+          id: 504
+        },
+        {
+          title: "No plastic trash",
+          completed: false,
+          id: 505,
+        },
+        {
+          title: "Use own bag",
+          completed: false,
+          id: 506
+        },
+        {
+          title: "Recycle",
+          completed: true,
+          id: 503,
+        },
+        {
+          title: "Plant a tree",
+          completed: true,
+          id: 502
+        }
+      ];
+
+  @HostListener('document:click', ['$event']) clickout(event) {
+    this.isHabitsPopUpOpen =  this.isHabitsPopUpOpen ? false : null;
+  }
 
   constructor(public translate: TranslateService,
-              public languageService: LanguageService) {
+              public languageService: LanguageService,
+              public habitAssignService: HabitAssignService) {
                 super(translate, languageService);
               }
 
@@ -21,4 +64,36 @@ export class CalendarComponent extends CalendarBaseComponent implements OnInit, 
     this.buildCalendar();
   }
 
+  public getFormatedData(dayItem) {
+    console.log(dayItem);
+    this.formatedData = `${dayItem.year}-${ dayItem.month + 1 < 10 ? "0" + dayItem.month : dayItem.month + 1}-${dayItem.numberOfDate < 10 ? "0" + dayItem.numberOfDate : dayItem.numberOfDate}`;
+  }
+
+  public toggleHabitsList(dayItem) {
+    this.getFormatedData(dayItem);
+    this.getActiveDateHabits(this.formatedData);
+    this.isHabitsPopUpOpen = !this.isHabitsPopUpOpen;
+    this.selectedDay = dayItem.numberOfDate;
+    console.log(dayItem.numberOfDate);
+    console.log(this.formatedData);
+    this.habitsCalendarSelectedDate = this.months[dayItem.month] + " " + dayItem.numberOfDate + ", " + dayItem.year;
+  }
+
+  public getActiveDateHabits(date) {
+    this.habitAssignService.getActiveDateHabits(date, this.language).subscribe( data => {
+      console.log(data);
+      this.habits2 = [];
+      data.forEach(item => {
+        this.habits2 = [...this.habits2, item.habit];
+      });
+      console.log(this.habits2);
+    })
+  }
+
+  // public toggleCompleteHabit(habit) {
+  //   console.log(`habitid:${habit.id} and formdata:${this.formatedData} and completed${habit.completed}`);
+  //   habit.completed = !habit.completed;
+  //   this.isChangingEnroll = true;
+  //   setTimeout(() => this.isChangingEnroll = false, 1000);
+  // }
 }
---
+++ src/app/image-pathes/calendar-icons.ts
@@ -0,0 +1,4 @@
+export const calendarIcons = {
+    plusCircle: 'assets/img/icon/plus-circle.png',
+    checkCircle: 'assets/img/icon/chek-circle.png'
+};
