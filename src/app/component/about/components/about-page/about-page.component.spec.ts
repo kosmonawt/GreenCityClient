098149b1bd1f09d6bbffb781416e55cb7e6cc069
JimHolman1199 <jimholman1199@gmail.com>
2020-12-08 00:33:40 +0200

    [TASK] add stubClass for translateService

--- src/app/component/about/components/about-page/about-page.component.spec.ts
+++ src/app/component/about/components/about-page/about-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Injectable } from '@angular/core';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LocalStorageService } from '@global-service/localstorage/local-storage.service';
@@ -9,33 +9,48 @@ import { BehaviorSubject, of, Subject } from 'rxjs';
 
 import { AboutPageComponent } from './about-page.component';
 
+@Injectable()
+class TranslationServiceStub {
+  public onLangChange = new EventEmitter<any>()
+  public onTranslationChange = new EventEmitter<any>()
+  public onDefaultLangChange = new EventEmitter<any>()
+  public addLangs(langs: string[]) { return }
+  public getLangs() { return ["en-us"] }
+  public getBrowserLang() { return "" }
+  public getBrowserCultureLang() { return "" }
+  public use(lang: string) { return null }
+  // tslint:disable-next-line:no-reserved-keywords
+  public get(key: any): any { return of(key) }
+  public setDefaultLang(){return true}
+}
+
 fdescribe('AboutPageComponent', () => {
   let component: AboutPageComponent;
   let fixture: ComponentFixture<AboutPageComponent>;
   const routerSpy = { navigate: jasmine.createSpy('navigate') };
   const mockLang = 'ru';
-
-  let translateServiceMock: TranslateService;
-  translateServiceMock = jasmine.createSpyObj('TranslateService', ['setDefaultLang']);
-  translateServiceMock.setDefaultLang = (lang: string) => of ();
-  translateServiceMock.get = () => of (true);
-  let localStorageServiceMock: LocalStorageService;
+  let localStorageServiceMock = new LocalStorageService();
+  
   localStorageServiceMock = jasmine.createSpyObj('LocalStorageService', ['userIdBehaviourSubject']);
   localStorageServiceMock.userIdBehaviourSubject = new BehaviorSubject(1111);
-  //localStorageServiceMock = jasmine.createSpyObj('LocalStorageService', ['languageSubject']);
   localStorageServiceMock.languageSubject = new Subject();
-  //localStorageServiceMock = jasmine.createSpyObj('LocalStorageService', ['getCurrentLanguage']);
   localStorageServiceMock.getCurrentLanguage = () => mockLang as Language;
-
-
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AboutPageComponent ],
-      imports: [RouterTestingModule.withRoutes([]), TranslateModule.forRoot()],
-      providers: [{provide: LocalStorageService, useValue: localStorageServiceMock},
-                  {provide: TranslateService, useValue: translateServiceMock}],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        { provide: LocalStorageService, useValue: localStorageServiceMock },
+        { provide: TranslateService, useClass: TranslationServiceStub }
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+        NO_ERRORS_SCHEMA
+      ]
     })
     .compileComponents();
   }));
@@ -46,7 +61,7 @@ fdescribe('AboutPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create',  () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
@@ -55,22 +70,22 @@ fdescribe('AboutPageComponent', () => {
     fixture.destroy();
   });
 
-  // it ('ngOnInit should called subscribeToLangChange method one time', () => {
-  //   const subscribeToLangChangeSpy = spyOn(component as any, 'subscribeToLangChange');
-  //   component.ngOnInit();
-  //   expect(subscribeToLangChangeSpy).toHaveBeenCalledTimes(1);
-  // });
+  it ('ngOnInit should called subscribeToLangChange method one time', () => {
+    const subscribeToLangChangeSpy = spyOn(component as any, 'subscribeToLangChange');
+    component.ngOnInit();
+    expect(subscribeToLangChangeSpy).toHaveBeenCalledTimes(1);
+  });
 
-  // it('should get userId', () => {
-  //   expect(localStorageServiceMock.userIdBehaviourSubject.value).toBe(1111);
-  // });
+  it('should get userId', () => {
+    expect(localStorageServiceMock.userIdBehaviourSubject.value).toBe(1111);
+  });
 
-  // it('should redirect to profile page', () => {
-  //   fixture.ngZone.run(() => {
-  //     component.navigateToHabit();
-  //     fixture.whenStable().then(() => {
-  //       expect(routerSpy.navigate).toBeDefined();
-  //     });
-  //   });
-  // });
+  it('should redirect to profile page', () => {
+    fixture.ngZone.run(() => {
+      component.navigateToHabit();
+      fixture.whenStable().then(() => {
+        expect(routerSpy.navigate).toBeDefined();
+      });
+    });
+  });
 });
