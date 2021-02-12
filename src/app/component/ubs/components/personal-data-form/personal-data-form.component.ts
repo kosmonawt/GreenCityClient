2dc79d5bb04184fdffadb1f26c157498a4573a4d
Volodymyr Lukashevych <volodia.lukashewwich@gmail.com>
2021-02-12 15:35:34 +0200

    fixed phone number

--- src/app/component/ubs/components/personal-data-form/personal-data-form.component.ts
+++ src/app/component/ubs/components/personal-data-form/personal-data-form.component.ts
@@ -43,7 +43,7 @@ export class PersonalDataFormComponent implements OnInit {
       ]],
       phoneNumber: ['+38 0', [
         Validators.required,
-        Validators.minLength(19)
+        Validators.minLength(12)
       ]],
       city: ['Київ', Validators.required],
       district: [null, Validators.required],
@@ -124,14 +124,11 @@ export class PersonalDataFormComponent implements OnInit {
   }
 
   submit(): void {
-    const phoneField = this.personalDataForm.get('phoneNumber');
-    const phoneValue = `${phoneField.value[6]}${phoneField.value[7]}${phoneField.value[10]}${phoneField.value[11]}${phoneField.value[12]}${phoneField.value[14]}${phoneField.value[15]}${phoneField.value[17]}${phoneField.value[18]}`;
-    
     const personalData: PersonalData = {
       firstName: this.personalDataForm.get('firstName').value,
       lastName: this.personalDataForm.get('lastName').value,
       email: this.personalDataForm.get('email').value,
-      phoneNumber: phoneValue,
+      phoneNumber: this.personalDataForm.get('phoneNumber').value.slice(3),
       city: this.personalDataForm.get('city').value,
       district: this.personalDataForm.get('district').value,
       street: this.personalDataForm.get('streetAndBuilding').value.split(', ')[0],
